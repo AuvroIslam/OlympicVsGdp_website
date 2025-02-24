77351433f906700d1030201ed3ea9d5cf80510f2
AuvroIslam <auvro2107024@stud.kuet.ac.bd>
2025-02-24 18:19:58 +0600

    added github link preoperly

--- src/Components/Setup/Setup.jsx
+++ src/Components/Setup/Setup.jsx
@@ -10,8 +10,8 @@ const Setup = () => {
                 <h1 className="text-3xl md:text-5xl font-bold text-[#443627]">📝 Setup Instructions:</h1>
                 <ul className="list-disc mx-4 md:mx-8 my-4 marker:text-[#443627]">
                     <li className="font-semibold ">Clone the repository, install dependencies, and run the model in Jupyter Notebooks or Google Colab for testing and further development.</li>
-                    <li className="font-semibold ">You can get the scrapped data <a className="link text-[#D98324]" href="https://github.com/AuvroIslam/Olympic-vs-Gdp/tree/main/data">here.</a></li>
-                    <li className="font-semibold ">For more details, check out the <a className="link text-[#D98324]" href="https://github.com/AuvroIslam/Olympic-vs-Gdp">full documentation.</a></li>
+                    <li className="font-semibold ">You can get the scrapped data <a className="link text-[#D98324]" href="https://github.com/AuvroIslam/Olympic_vs_Gdp/tree/main/data">here.</a></li>
+                    <li className="font-semibold ">For more details, check out the <a className="link text-[#D98324]" href="https://github.com/AuvroIslam/Olympic_vs_Gdp">full documentation.</a></li>
                     
                 </ul>
                
